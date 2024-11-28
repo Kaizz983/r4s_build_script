223cdaa82b00066feebc5ae519cadf797126a223
Roxy09099 <longtoan99@gmail.com>
2024-11-28 16:13:06 +0700

    Update 04-fix_kmod.sh

--- openwrt/scripts/04-fix_kmod.sh
+++ openwrt/scripts/04-fix_kmod.sh
@@ -57,7 +57,7 @@ curl -s $mirror/openwrt/patch/packages-patches/xtables-addons/302-fix-build-for-
 pushd feeds/telephony
   # dahdi-linux
   rm -rf libs/dahdi-linux
-  git clone https://$github/sbwml/feeds_telephony_libs_dahdi-linux libs/dahdi-linux
+  git clone --depth=1 https://$github/sbwml/feeds_telephony_libs_dahdi-linux libs/dahdi-linux
 popd
 
 # routing - batman-adv fix build with linux-6.12
@@ -73,16 +73,16 @@ fi
 if [ "$KERNEL_CLANG_LTO" = "y" ]; then
     # xtables-addons module
     rm -rf feeds/packages/net/xtables-addons
-    git clone https://$github/sbwml/kmod_packages_net_xtables-addons feeds/packages/net/xtables-addons
+    git clone --depth=1 https://$github/sbwml/kmod_packages_net_xtables-addons feeds/packages/net/xtables-addons
     # netatop
     sed -i 's/$(MAKE)/$(KERNEL_MAKE)/g' feeds/packages/admin/netatop/Makefile
     curl -s $mirror/openwrt/patch/packages-patches/clang/netatop/900-fix-build-with-clang.patch > feeds/packages/admin/netatop/patches/900-fix-build-with-clang.patch
     # dmx_usb_module
     rm -rf feeds/packages/libs/dmx_usb_module
-    git clone https://$gitea/sbwml/feeds_packages_libs_dmx_usb_module feeds/packages/libs/dmx_usb_module
+    git clone --depth=1 https://$gitea/sbwml/feeds_packages_libs_dmx_usb_module feeds/packages/libs/dmx_usb_module
     # macremapper
     curl -s $mirror/openwrt/patch/packages-patches/clang/macremapper/100-macremapper-fix-clang-build.patch | patch -p1
     # coova-chilli module
     rm -rf feeds/packages/net/coova-chilli
-    git clone https://$github/sbwml/kmod_packages_net_coova-chilli feeds/packages/net/coova-chilli
+    git clone --depth=1 https://$github/sbwml/kmod_packages_net_coova-chilli feeds/packages/net/coova-chilli
 fi
